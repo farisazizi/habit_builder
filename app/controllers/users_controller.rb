ab2a3221b0a9a335aea5f018143f6f14f1ec699c
liteen <liteen.eh@gmail.com>
2015-06-11 16:56:02 +0000

    complete password field, signup backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
     @user = User.new
   end
   
-   def create
+  def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the habit_builder!"
       redirect_to @user
     else
       render 'new'
@@ -15,6 +21,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
