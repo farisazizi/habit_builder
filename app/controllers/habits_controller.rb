b49b32868cae1c24cf042f611e9ae23f68afe868
Alden Cheong <gze.xie@gmail.com>
2015-08-24 12:10:09 +0000

    Update habit creation

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -63,6 +63,7 @@ class HabitsController < ApplicationController
   end
 
   private
+  
     # Use callbacks to share common setup or constraints between actions.
     def set_habit
       @habit = Habit.find(params[:id])
